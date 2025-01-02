4905431973045381f564fa4c4b657fc77cc597d0
Rebecca Riffle <rebeccaeriffle@gmail.com>
2025-01-02 15:54:55 -0800

    Completed NumberPractice and added floats and printing to toRefresh

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,17 +1,26 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
+    float negativeFloat = -0.12345f;
 
     // Create an int with a positive value and assign it to a variable
+    int positiveInteger = 42;
 
     // Use the modulo % operator to find the remainder when the int is divided by 3
+    int remainder = positiveInteger % 3;
 
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
+    if (remainder % 2 == 0) {
+      System.out.println("Even");
+    } else if (remainder % 2 != 0) {
+      System.out.println("Odd");
+    };
 
     // Divide the number by another number using integer division
+    int dividend = remainder / 2;
 
     /*
      * Reminder!
--- toRefresh.md
+++ toRefresh.md
@@ -2,4 +2,15 @@
 
 As you work through this exercise, write down anything that you needed to look up or struggled to remember here. It can be just a word or two (e.g. "joining strings"). You can use this as a guide of what to make extra sure you're refreshed on before exams and interviews.
 
+## Number Practice
+### Floats
+Create a float using the "float" keyword and an "f" at the end of the number.
+```
+float exampleFloat = 0.123f;
+```
+### Print to console
+To print in Java, call the println() method on System.out.
+```
+System.out.println("")
+```
 - 
