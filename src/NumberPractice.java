1ac7783a01510e19547b95b5a15a80bd6742ae4a
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-01-03 11:03:21 -0800

    Added space for better readability

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -2,10 +2,13 @@ public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
     float negativeVal = -123f;
+
     // Create an int with a positive value and assign it to a variable
     int positiveVal = 123;
+
     // Use the modulo % operator to find the remainder when the int is divided by 3
     int remainder = positiveVal % 3;
+
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
@@ -15,9 +18,11 @@ public class NumberPractice {
     } else {
       System.out.println("ODD");
     }
+
     // Divide the number by another number using integer division
     int division = positiveVal / 3;
     System.out.println(division);
+    
     /*
      * Reminder!
      * 
