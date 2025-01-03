7fe3cdb6d8a327086485587bf37d961eec2ccc5f
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-01-02 16:50:13 -0800

    Started a third set of problems, created a basic String array and popular it with 4 values

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,9 +1,13 @@
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
-
+    String[] myArr = new String[4];
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
+    myArr[0] = "Hello";
+    myArr[1] = "Bonjour";
+    myArr[2] = "Hola";
+    myArr[3] = "Salam";
 
     // Get the value of the array at index 2
 
