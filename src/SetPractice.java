f540700174c33028338867251fd82b660fe08902
Madeleine Chance <mechance782@gmail.com>
2025-01-03 14:16:01 -0800

    finished set practice

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,18 +1,25 @@
+import java.util.*;
+
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
-
+    Set<String> mySet = new HashSet<>();
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
-
+    mySet.add("hi");
+    mySet.add("hello");
+    mySet.add("howdy");
     // Check whether the Set contains a given String
-
+    System.out.println(mySet.contains("howdy"));
     // Remove an element from the Set
-
+    mySet.remove("hi");
+    System.out.println(mySet);
     // Get the size of the Set
-
+    System.out.println(mySet.size());
     // Iterate over the elements of the Set, printing each one on a separate line
-
+    for(String string : mySet){
+      System.out.println(string);
+    }
     /*
      * Warning!
      * 
