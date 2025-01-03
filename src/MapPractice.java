dbdf939633bfc5f8b34bbdd6be6159cbf8ecf9bf
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-01-02 17:32:52 -0800

    Wrote a loop to iterate over the Keys for the HashMap and additional print statements

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -19,11 +19,13 @@ public class MapPractice {
     myMap.put("Alice", 76);
     System.out.println("Updated value for 'Alice' key: " + myMap.get("Alice"));
     // Check whether the Map contains a given key
-
+    System.out.println("Does the Map contains the key 'Edward'? " + myMap.containsKey("Edward"));
     // Check whether the Map contains a given value
-
+    System.out.println("Does the Map contains the value 88? " + myMap.containsValue(88));
     // Iterate over the keys of the Map, printing each key
-
+    for (String key : myMap.keySet()) {
+      System.out.println(key);
+    }
     // Iterate over the values of the map, printing each value
 
     // Iterate over the entries in the map, printing each key and value
