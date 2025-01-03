fb8ccb98ccaaf2ce9be9f0d1c9687da1e7ef246a
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-01-03 08:57:27 -0800

    Added traditional loop to print out each item

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -13,7 +13,9 @@ public class ArrayPractice {
     // Get the length of the array
     System.out.println(arrayFour.length);
     // Iterate over the array using a traditional for loop and print out each item
-
+    for (int i = 0; i < arrayFour.length; i++) {
+      System.out.println(arrayFour[i]);
+    }
     // Iterate over the array using a for-each loop and print out each item
 
     /*
