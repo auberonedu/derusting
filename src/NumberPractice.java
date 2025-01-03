a9c1a98645c5a76910d6c8df6b6e6db9706ae775
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-01-03 10:55:49 -0800

    Addec and created a float with negative value, created an int with positive value, and ofound remainder of positiveVal

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,11 +1,11 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
-
+    float negativeVal = -123f;
     // Create an int with a positive value and assign it to a variable
-
+    int positiveVal = 123;
     // Use the modulo % operator to find the remainder when the int is divided by 3
-
+    int remainder = positiveVal % 3;
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
