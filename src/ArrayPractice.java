7393237c8528e5be7547264668fe5ff83075c182
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-01-03 08:41:24 -0800

    Created a new array of strings with size 4

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,7 +1,7 @@
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
-
+      String[] arrayFour = new String[4];
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
 
