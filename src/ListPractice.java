e8a40d3f9be5dda5273e3dc1f5882ab423d228cf
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-01-03 09:09:33 -0800

    Added import for ArrayList and created new ArrayList called stringList

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
-
+    ArrayList<Integer> stringList = new ArrayList<>();
     // Add 3 elements to the list (OK to do one-by-one)
 
     // Print the element at index 1
