bb14bd1243ca055aa23106b692c045f856f4982d
Jameson Farmer <thejamesonfarmer@gmail.com>
2025-01-03 00:08:08 -0800

    Completed Number and Set Practice

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,18 +1,29 @@
+import java.util.HashSet;
+
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
-
+    HashSet<String> set = new HashSet<String>();
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
-
+    set.add("Jameson");
+    set.add("Jace");
+    set.add("Lexie");
     // Check whether the Set contains a given String
-
+    String test = "Jameson";
+    if(set.contains(test)){
+      System.out.println("Set contains " + test);
+    }else{
+      System.out.println("Set does not contain " + test);
+    }
     // Remove an element from the Set
-
+    set.remove("Jace");
     // Get the size of the Set
-
+    System.out.println(set.size());
     // Iterate over the elements of the Set, printing each one on a separate line
-
+    for (String i : set) {
+      System.out.println(i);
+    }
     /*
      * Warning!
      * 
